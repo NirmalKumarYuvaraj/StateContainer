e03ddfbd2b94add82b4f1785fb23a142a578a079
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2023-07-11 19:03:52 +0530

    Updated code

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Syncfusion.Maui.Core;
+using System.ComponentModel;
+using Xamarin.Google.Crypto.Tink.Proto;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+        viewModel.CurrentState = "Loaded";
+	}
+
+}
+
+public class ViewModel1 : INotifyPropertyChanged
+{
+       
+    private string _currentState = "Loaded";
+    public string CurrentState
+    {
+        get => _currentState;
+        set
+        {
+            _currentState = value;
+            NotifyPropertyChanged(nameof(CurrentState));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void NotifyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
+
+public class CustomBoxView : BoxView
+{
+    public CustomBoxView()
+    {
+        BackgroundColor = Colors.Red;
+    }
+    protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
+    {
+        return base.MeasureOverride(widthConstraint, heightConstraint);
+    }
+
+    protected override Size ArrangeOverride(Rect bounds)
+    {
+        return base.ArrangeOverride(bounds);
+    }
+}
+
